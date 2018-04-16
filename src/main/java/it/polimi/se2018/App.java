74078eb25756e9614678ebed9d95f382c488a50f
cristiangiann <cristian.giannetti@mail.polimi.it>
2018-04-16 16:04:39 +0200

    Commit iniziale

---
+++ src/main/java/it/polimi/se2018/App.java
@@ -0,0 +1,15 @@
+package it.polimi.se2018;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int pippo;
+        System.out.println( "Hello World!" );
+    }
+
+}
