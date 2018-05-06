3ec25d2315552f0c5e7d1d0101bb1331fc8431e3
sissifranz <silvia1.franzini@mail.polimi.it>
2018-05-06 17:52:51 +0200

    Enumeration used to save all the game's colours

---
+++ src/main/java/it/polimi/se2018/model/ColourEnum.java
@@ -0,0 +1,9 @@
+package it.polimi.se2018.model;
+
+/**
+ * Enum containing all the colours that can ben seen in the game
+ * @author Silvia Franzini
+ */
+public enum ColourEnum {
+    BLUE, GREEN, PURPLE, RED, YELLOW
+}
