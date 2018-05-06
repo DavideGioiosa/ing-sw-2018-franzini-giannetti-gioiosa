581bf0e6b7e0d8640eb55e07d7d4c5b5ea1a0712
sissifranz <silvia1.franzini@mail.polimi.it>
2018-05-06 17:52:00 +0200

    Implementation of Private Objective Cards: getter and a score calculation method

---
+++ src/main/java/it/polimi/se2018/model/cards/PrivateObjCard.java
@@ -0,0 +1,38 @@
+package it.polimi.se2018.model.cards;
+
+/**
+ * Private Objective Cards
+ * @author Silvia Franzini
+ */
+public class PrivateObjCard extends Card {
+    private ColourEnum colour;
+
+    public PrivateObjCard(int id, String name, String description, ColourEnum colour) {
+        super(id, name, description);
+        this.colour = colour;
+    }
+
+    /**
+     * Returns the private objective of the player
+      * @return a colour from the ColourEnum
+     */
+    public ColourEnum getColour() {
+        return colour;
+    }
+
+    /**
+     * Method used to return the score due to the Private Objective Card
+     * @param schemaCard, player's window pattern card
+     * @return an int representing the score achieved
+     */
+    public int getScore(SchemaCard schemaCard){
+        int sum = 0;
+        for(Cell cell : schemaCard.getCellList()) {
+
+            if(cell.getDice().getColour().equals(colour)){
+                sum += cell.getDice().getValue();
+            }
+        }
+        return sum;
+    }
+}
