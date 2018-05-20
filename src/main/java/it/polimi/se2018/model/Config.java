9b24aa9b4c9421679bffcde20e38a3ac11ebcf0f
cristiangiann <cristian.giannetti@mail.polimi.it>
2018-05-20 23:15:16 +0200

    UML_MVC updated to 1.2 and UML Connection created

--- src/main/java/it/polimi/se2018/model/Config.java
+++ src/main/java/it/polimi/se2018/model/Config.java
@@ -11,6 +11,12 @@ public class Config {
     public static final int NUMBEROFPUBLICOBJCARD = 10;
     public static final int NUMBEROFPRIVATEOBJCARD = 5;
 
+    public static final int IDFIRSTPUBLICOBJCARD = 100;
+    public static final int IDFIRSTPRIVATEOBJCARD = 200;
+    public static final int IDFIRSTTOOLCARDS = 300;
+    public static final int IDFIRSTSCHEMACARD = 400;
+
+
     public static final int NUMBEROFSCHEMAROW = 4;
     public static final int NUMBEROFSCHEMACOL = 5;
 
