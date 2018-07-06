1581fda1cddf4f189a1e0bf798065972792d97e5
DavideGioiosa <davide.gioiosa@mail.polimi.it>
2018-07-06 23:26:58 +0200

    Added tests and update fxml gameTable

--- src/test/java/it/polimi/se2018/model/GameBoardTest.java
+++ src/test/java/it/polimi/se2018/model/GameBoardTest.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 /**
@@ -159,6 +160,57 @@ public class GameBoardTest {
         }catch(NullPointerException e){}
     }
 
+    @Test
+    public void cloneGameBoard_shouldCloneCorrectly (){
+        GameBoard gameBoard = newGameBoard();
+
+        GameBoard gameBoardClone = gameBoard.getClone();
+
+        boolean isEqualBoardDice = true;
+
+        for(int i=0; i<gameBoard.getBoardDice().getDieList().size(); i++) {
+            if(gameBoard.getBoardDice().getDieList().get(i) != gameBoardClone.getBoardDice().getDieList().get(i)){
+                isEqualBoardDice = false;
+            }
+        }
+
+        boolean isEqualTrackBoardDice = true;
+        for(int i=0; i< gameBoard.getTrackBoardDice().getDiceList().size(); i++){
+            if(gameBoard.getTrackBoardDice().getDiceList().get(i) != gameBoardClone.getTrackBoardDice().getDiceList().get(i)){
+                isEqualTrackBoardDice = false;
+            }
+        }
+
+        boolean isEqualToolCardList = true;
+        for(int i=0; i<gameBoard.getCardOnBoard().getToolCardList().size(); i++){
+            if(gameBoard.getCardOnBoard().getToolCardList().get(i) != gameBoard.getCardOnBoard().getToolCardList().get(i)){
+                isEqualToolCardList = false;
+            }
+        }
+
+        boolean isEqualPublicCardList = true;
+        for(int i=0; i<gameBoard.getCardOnBoard().getPublicObjCardList().size(); i++){
+            if(gameBoard.getCardOnBoard().getPublicObjCardList().get(i).getBonus() != gameBoardClone.getCardOnBoard().getPublicObjCardList().get(i).getBonus()){
+                isEqualPublicCardList = false;
+            }
+        }
+
+        boolean isEqualPlayerList = true;
+        for(int i=0; i<gameBoard.getPlayerList().size(); i++){
+            if(gameBoard.getPlayerList().get(i).getNickname() != gameBoardClone.getPlayerList().get(i).getNickname()){
+                isEqualPlayerList = false;
+            }
+        }
+
+
+        assertTrue(isEqualBoardDice);
+        assertTrue(isEqualTrackBoardDice);
+        assertTrue(isEqualToolCardList);
+        assertTrue(isEqualPublicCardList);
+        assertTrue(isEqualPlayerList);
+    }
+
+
     /**
      * Creates a GameBoard with valid parameters
      * @return new GameBoard for tests
@@ -167,5 +219,6 @@ public class GameBoardTest {
         setUp();
         return new GameBoard(playerList, bagDice,boardDice, trackBoardDice, boardCard, privatePlayerList);
     }
+
 
 }
